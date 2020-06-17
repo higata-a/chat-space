5429101e5e5fa589279c6ffa73ed0292a532b14c
higata-a <64731787+higata-a@users.noreply.github.com>
2020-06-17 14:53:46 +0900

    f

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,11 +8,21 @@ $(function() {
       dataType: "json"
     })
       .done(function(users) {
-        console.log("成功です");
-        console.log(users.url)
+        var html = `
+        <div class="chat-group-user clearfix">
+          <p class="chat-group-user__name">ユーザー名</p>
+          <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+        </div>
+        `
+        $('#chat-group-users.js-add-user').append(html);
+        console.log("o")
       })
       .fail(function() {
-        console.log("失敗です");
+        var html = `
+        <div class="chat-group-user clearfix">
+         <p class="chat-group-user__name">ユーザーが見つかりません</p>
+        </div>`
+        $('.messages').append(html);
       });
   });
 });
