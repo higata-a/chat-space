62aa81c73e0b17195379d685a93c3e83bac477fb
higata-a <64731787+higata-a@users.noreply.github.com>
2020-06-17 18:00:55 +0900

    dd

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -8,21 +8,34 @@ $(function() {
       dataType: "json"
     })
       .done(function(users) {
-        var html = `
-        <div class="chat-group-user clearfix">
-          <p class="chat-group-user__name">ユーザー名</p>
-          <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
-        </div>
-        `
-        $('#chat-group-users.js-add-user').append(html);
-        console.log("o")
-      })
+        $('#chat-group-users.js-add-user').empty();
+        console.log(user)
+
+        if (input.length !== 0) {
+          users.forEach(function(user){
+            console.log(user);
+            var html = `
+            <div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">ユーザー名</p>
+              <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+            </div>
+            `
+            $('#chat-group-users.js-add-user').append(html);
+
+          });
+        }
+        else {
+        }
+
+        })
+
+
       .fail(function() {
         var html = `
         <div class="chat-group-user clearfix">
          <p class="chat-group-user__name">ユーザーが見つかりません</p>
         </div>`
-        $('.messages').append(html);
+        $('#chat-group-users.js-add-user').append(html);
       });
   });
 });
